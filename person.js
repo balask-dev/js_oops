33434197b0969ae63e1b631894ac49c982c00bb9
Bala <92292670+balask-dev@users.noreply.github.com>
2022-05-30 14:31:31 +0530

    Create person.js

---
+++ person.js
@@ -0,0 +1,16 @@
+class Person{
+    constructor(name, age, city, profession, contry,education){
+        this.name = name;
+        this.age = age;
+        this.city = city;
+        this.profession = profession;
+        this.contry = contry;
+        this.education = education;
+     }
+    
+}
+
+var person  = new Person ("faiz" , "23",  "dubai" ,"engineer" , "dubai ","ECE" );
+var personb  = new Person ("bala" , "23",  "chennai" ,"engineer" , "india ","ECE" );
+
+ 
